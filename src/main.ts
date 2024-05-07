0c9c93f1863c303de3a8820837db01911cacbd6a
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2024-05-07 01:49:04 -0400

    Añadir pnpm-lock.yaml y package-lock.json al .gitignore

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { json } from 'express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true }); // Cors para que se pueda acceder desde cualquier lado
+
+  app.use(json({ limit: '500mb' })); // Tamaño máximo de los datos
+
+  app.setGlobalPrefix("api/v1");
+
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  );
+
+  const config = new DocumentBuilder() // Documentación
+    .addBearerAuth()
+    .setTitle('HealTrack API')
+    .setDescription('Esta es la api de HealTrack')
+    .addTag('users')
+    .addTag('auth')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config); // Documentación
+  SwaggerModule.setup('api', app, document); // Documentación
+
+  await app.listen(PORT);
 }
 bootstrap();
