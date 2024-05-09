60707bcbcd904eac1bf0b7f9cad013f543fc13f8
Emmanuel Salcedo <davidsalcedo388@gmail.com>
2024-05-09 08:55:39 -0400

    fix(employee): Actualizacion de los DTO de empleados

--- src/core/employees/dto/update-employee.dto.ts
+++ src/core/employees/dto/update-employee.dto.ts
@@ -1,17 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { isUniqueDb } from "@youba/nestjs-dbvalidator";
-import { IsEmail, IsIn, IsOptional, IsString } from "class-validator";
+import { IsEmail, IsEnum, IsIn, IsOptional, IsString } from "class-validator";
 import { EmployeeRole } from "src/constants";
 
 export class UpdateEmployeeDto {
     
+    @ApiProperty({ example: "Emmanuel"})
     @IsString()
     @IsOptional()
     name: string;
 
+    @ApiProperty({ example: "Salcedo"})
     @IsString()
     @IsOptional()
     lastname: string;
 
+    @ApiProperty({ example: "admin@gmail.com"})
     @isUniqueDb({
         table: 'employee',
         column: 'email',
@@ -21,14 +25,16 @@ export class UpdateEmployeeDto {
     @IsOptional()
     email: string;
 
+    @ApiProperty({ example: "V30109748"})
     @IsString()
     @IsOptional()
     identification: string;
 
     @IsOptional()
     isVerify: boolean;
-
-    @IsIn([EmployeeRole.ADMIN, EmployeeRole.SPECIALIST, EmployeeRole.ASSISTANT])
+    
+    @ApiProperty({ enum: EmployeeRole, example: EmployeeRole.ADMIN})
+    @IsEnum(EmployeeRole, { message: 'Rol invalido' })
     @IsOptional()
     role: EmployeeRole;
 }
--- src/core/employees/employees.controller.ts
+++ src/core/employees/employees.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Response } from '@nestjs/common';
 import { EmployeesService } from './service/employees.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
 import { ApiTags } from '@nestjs/swagger';
+import * as express from 'express';
+
 
 @ApiTags('employees')
 @Controller('employees')
@@ -10,27 +12,36 @@ export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
 
   @Post()
-  create(@Body() createEmployeeDto: CreateEmployeeDto) {
+  async create(@Body() createEmployeeDto: CreateEmployeeDto) {
     return this.employeesService.create(createEmployeeDto);
   }
 
   @Get()
-  findAll() {
+  async findAll() {
     return this.employeesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  async findOne(@Param('id') id: string) {
     return this.employeesService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateEmployeeDto: UpdateEmployeeDto) {
-    return this.employeesService.update(+id, updateEmployeeDto);
+  async update(@Param('id') id: string, @Body() updateEmployeeDto: UpdateEmployeeDto, @Response() response: express.Response,) {
+    await this.employeesService.update(+id, updateEmployeeDto);
+
+    return response.status(200).json({
+      message: "Se ha modificado correctamente el empleado"
+    })
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.employeesService.remove(+id);
+  async remove(@Param('id') id: string, @Response() response: express.Response,) {
+    await this.employeesService.remove(+id);
+
+    return response.status(200).json({
+      message: "Se ha eliminado correctamente el empleado"
+    })
+
   }
 }
