9f7d57816d0e1657d780a65ae15287156fadd9c6
Emmanuel Salcedo <73862929+HopeAero@users.noreply.github.com>
2024-05-14 18:07:00 -0400

    Update chat.gateway.ts

--- src/core/chat/chat.gateway.ts
+++ src/core/chat/chat.gateway.ts
@@ -5,29 +5,29 @@ import {
   MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
-} from '@nestjs/websockets';
-import { Logger } from '@nestjs/common';
-import { Server, Socket } from 'socket.io';
-import { AddMessageDto } from './dto/addMessage.dto';
-import { CORS } from '@src/constants';
+} from "@nestjs/websockets";
+import { Logger } from "@nestjs/common";
+import { Server, Socket } from "socket.io";
+import { AddMessageDto } from "./dto/addMessage.dto";
+import { CORS } from "@src/constants";
 
-@WebSocketGateway({ cors: CORS})
+@WebSocketGateway({ cors: CORS })
 export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
-  private logger = new Logger('ChatGateway');
+  private logger = new Logger("ChatGateway");
 
-  @SubscribeMessage('joinRoom')
+  @SubscribeMessage("joinRoom")
   handleJoinRoom(socket: Socket, @MessageBody() room: string) {
     socket.join(room);
     this.logger.log(`Socket ${socket.id} joined room ${room}`);
   }
 
-  @SubscribeMessage('chat')
+  @SubscribeMessage("chat")
   handleMessage(@MessageBody() payload: AddMessageDto): AddMessageDto {
     this.logger.log(`Message received: ${payload.author} - ${payload.body}`);
-    this.server.emit('chat', payload);
+    this.server.emit("chat", payload);
     return payload;
   }
 
